c1dd2f11445046477973f43017aa2ad04bde849c
Robin Neal <howdy@robinneal.co.uk>
2022-10-01 13:36:52 +0100

    plot selection

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,24 +5,7 @@ extends Spatial
 # var a = 2
 # var b = "text"
 var ray_length = 10
+var selected: Plot
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#var target = get_object_under_mouse();
-	#target.
-	#print(target)
+func select_plot(plot):
 	pass
-
-func get_object_under_mouse():
-	var mouse_pos = get_viewport().get_mouse_position()
-	var camera = get_node("/root/Game/Camera")
-	var ray_from = camera.project_ray_origin(mouse_pos)
-	var ray_to = ray_from + camera.project_ray_normal(mouse_pos) * ray_length
-	var space_state = get_world().direct_space_state
-	var selection = space_state.intersect_ray(ray_from, ray_to)
-	return selection
--- scenes/grid/Plot.gd
+++ scenes/grid/Plot.gd
@@ -1,4 +1,5 @@
 extends Spatial
+class_name Plot
 
 var selected = false;
 
@@ -7,9 +8,17 @@ func _ready():
 	var body = get_node("StaticBody")
 	body.connect("mouse_entered", self, "_on_Body_mouse_entered")
 	body.connect("mouse_exited", self, "_on_Body_mouse_exited")
+	body.connect("input_event", self, "_on_Body_input_event")
 
 func _on_Body_mouse_entered():
-	self.translate(Vector3(0, 0.2, 0))
+	if !selected:
+		self.translate(Vector3(0, 0.2, 0))
 	
 func _on_Body_mouse_exited():
-	self.translate(Vector3(0, -0.2, 0))
+	if !selected:
+		self.translate(Vector3(0, -0.2, 0))
+
+func _on_Body_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			selected = !selected
