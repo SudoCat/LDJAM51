c1c15dff539832db57603962cc7f6a996f59a440
Robin Neal <howdy@robinneal.co.uk>
2022-10-01 21:45:13 +0100

    duplicate and shuffle deck

--- resource_types/actor.gd
+++ resource_types/actor.gd
@@ -13,8 +13,10 @@ func _init(p_name = "card", p_image = null, p_deck = []):
 	deck = p_deck
 
 func shuffle():
-	active_deck = deck
+	active_deck = deck.duplicate()
 	active_deck.shuffle()
 
 func draw():
+	if active_deck.empty():
+		shuffle()
 	return active_deck.pop_back()
