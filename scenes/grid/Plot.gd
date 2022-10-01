cb15d060e2c4e6d4a456333e0d81dde43730d578
Robin Neal <howdy@robinneal.co.uk>
2022-10-01 14:57:26 +0100

    prettier highlight

--- scenes/grid/Plot.gd
+++ scenes/grid/Plot.gd
@@ -2,18 +2,23 @@ extends Spatial
 class_name Plot
 
 var selected = false
+var tween: Tween
 var body: StaticBody
 var mesh: MeshInstance
 var mat: SpatialMaterial
 var base_color: Color
-export var highlight_color: Color
+var pos: Vector3
+export var multiplier = 1.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	tween = $Tween
 	mesh = $tmpParent/grass
 	body = $StaticBody
-	mat = mesh.get_surface_material(0)
+	mat = mesh.get_surface_material(1).duplicate()
+	mesh.set_surface_material(1, mat)
 	base_color = mat.albedo_color
+	pos = transform.origin
 	body.connect("mouse_entered", self, "_on_Body_mouse_entered")
 	body.connect("mouse_exited", self, "_on_Body_mouse_exited")
 	body.connect("input_event", self, "_on_Body_input_event")
@@ -32,9 +37,21 @@ func _on_Body_input_event(camera, event, position, normal, shape_idx):
 			selected = !selected;
 			
 func focus():
-	translate(Vector3(0, 0.2, 0))
-	mat.albedo_color = highlight_color
+	tween.interpolate_property(
+		self, "translation:y", 
+		0, 0.4,
+		0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+	)
+	tween.start()
+	mesh.get_active_material(0).albedo_color *= multiplier
+	mesh.get_active_material(1).albedo_color *= multiplier
 
 func blur():
-	translate(Vector3(0, -0.2, 0))
-	mat.albedo_color = base_color
+	tween.interpolate_property(
+		self, "translation:y", 
+		self.translation.y, 0, 
+		0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+	)
+	tween.start()
+	mesh.get_active_material(0).albedo_color /= multiplier
+	mesh.get_active_material(1).albedo_color /= multiplier
