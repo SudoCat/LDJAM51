f76ac433e7aad5315d5f7ed7c126d5a3a5a8a7ba
Robin Neal <howdy@robinneal.co.uk>
2022-10-03 22:07:10 +0100

    UI pass and more just so much oh god my brain

--- resource_types/card.gd
+++ resource_types/card.gd
@@ -2,14 +2,16 @@ extends Resource
 class_name Card
 
 export(String) var name
+export(String, MULTILINE) var description
 export(Texture) var card_image
 export(PackedScene) var building
 export(Array,Resource) var effects
 export(Array,String) var tags
 export(int) var base_points
 
-func _init(p_name = "card", p_card_image = null, p_building = null, p_effects = [], p_tags = [], p_base_points = 0):
+func _init(p_name = "card", p_description = "", p_card_image = null, p_building = null, p_effects = [], p_tags = [], p_base_points = 0):
 	name = p_name
+	description = p_description
 	card_image = p_card_image
 	building = p_building
 	effects = p_effects
--- scenes/NextDistrict.gd
+++ scenes/NextDistrict.gd
@@ -1,13 +1,5 @@
 extends Button
 
 func _ready():
-	if name == "NextDistrict":
-		connect("pressed", get_node("/root/Game"), "_on_NextDistrict_pressed")
-	elif name == "Quit":
-		connect("pressed", self, "_on_Self_pressed")
-
-func _on_Self_pressed():
-	var credits = get_node("../../Credits")
-	credits.show()
-	credits.get_node("Summary").text = ""
+	connect("pressed", get_node("/root/Game"), "_on_NextDistrict_pressed")
 	
