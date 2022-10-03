b718d30f34c1cf98592733ba072d7b79b67264c4
Robin Neal <howdy@robinneal.co.uk>
2022-10-03 19:32:57 +0100

    woops!

--- scenes/NextDistrict.gd
+++ scenes/NextDistrict.gd
@@ -1,10 +1,10 @@
 extends Button
 
 func _ready():
-	connect("pressed", get_node("/root/Game"), "_on_Self_pressed")
+	connect("pressed", self, "_on_Self_pressed")
 
 func _on_Self_pressed():
-	var credits = get_node("/root/Game/Interface/Credits")
+	var credits = get_node("../../Credits")
 	credits.show()
 	credits.get_node("Summary").text = ""
 	
